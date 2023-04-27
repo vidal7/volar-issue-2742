217c8f7e1b208888b2eb9302f8b4b50a0ca33d3a
Vincent Dallaire <vincent.dallaire@dti.ulaval.ca>
2023-04-27 10:36:37 -0400

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<template>
+    	<p-component-issue></p-component-issue>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PComponentIssue from './p-component-issue.vue';
+
+export default defineComponent({
+    components: { PComponentIssue }
+});
+</script>
---
+++ src/p-component-issue.vue
@@ -0,0 +1,11 @@
+<template>
+    <a href="https://github.com/vuejs/language-tools/issues/2742">Issue 2742</a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'PComponentIssue'
+});
+</script>
