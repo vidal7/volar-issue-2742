af3a5a6c03314206c9e95765a25d6dbe51e74706
Vincent Dallaire <vincent.dallaire@dti.ulaval.ca>
2023-04-28 07:56:08 -0400

    Fix package-lock

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,14 @@
 <template>
-    	<p-component-issue></p-component-issue>
+    <p-component-issue :prop-obligatoire="2"
+                       :prop-optionnelle="'abc'"
+                       :prop-optionnelle-avec-defaut="true"></p-component-issue>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import PComponentIssue from './p-component-issue.vue';
+    import { defineComponent } from 'vue';
+    import PComponentIssue from './p-component-issue.vue';
 
-export default defineComponent({
-    components: { PComponentIssue }
-});
+    export default defineComponent({
+        components: { PComponentIssue }
+    });
 </script>
